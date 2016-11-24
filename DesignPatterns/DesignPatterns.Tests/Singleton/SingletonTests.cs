c35c26b20735e7ed8f10baf140c806c80f62dadf
anatolie-darii <anatolie.darii@outlook.com>
2016-11-24 15:03:47 +0200

    Initial commit of the solution structure

---
+++ DesignPatterns/DesignPatterns.Tests/Singleton/SingletonTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Tests.Singleton
+{
+    [TestClass]
+    class SingletonTests
+    {
+        [TestMethod]
+        public void ShouldCreateOnlyOneInstance()
+        {
+
+        }
+    }
+}
