ad8f6fd91fd52eca78e23b8b30c510e49002ddf6
anatolie-darii <anatolie.darii@outlook.com>
2016-12-08 19:10:30 +0200

    Added code logic to test the patterns

--- DesignPatterns/DesignPatternsClient/Program.cs
+++ DesignPatterns/DesignPatternsClient/Program.cs
@@ -3,12 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using DesignPatternsNet4.ChainOfResponsibility;
+using DesignPatternsNet4.Proxy;
+using DesignPatternsNet4.Singleton;
+
 namespace DesignPatternsClient
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello there design patterns!");
+            Console.Write("Testing singleton...");
+            var instance1 = Singleton.Instance;
+            var instance2 = Singleton.Instance;
+            var singletonMessage = instance1 == instance2 ? "equal" : "not equal";
+            Console.WriteLine($"Singleton instances are {singletonMessage}");
+
+            Console.WriteLine("Testing proxy");
+            var proxyResult = new Proxy().DoAction("Test action");
+            Console.WriteLine($"Message from proxy - {proxyResult}");
+
+            Console.WriteLine("Testing chain of responsibility");
+            var handleIncidentResult = new Level1Support().HandleIncident(2);
+            Console.WriteLine($"Request - {handleIncidentResult}");
+            Console.ReadLine();
         }
     }
 }
